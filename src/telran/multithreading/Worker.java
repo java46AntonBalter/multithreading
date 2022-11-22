dcd3b0650d76c2d3a3bcce9d76c8c79c88877241
Anton Balter <antbalter@gmail.com>
2022-11-22 19:54:19 +0200

    hw42 solution

--- src/telran/multithreading/Worker.java
+++ src/telran/multithreading/Worker.java
@@ -1,5 +1,47 @@
 package telran.multithreading;
 
-public class Worker {
+public class Worker extends Thread {
+	static Object resource1 = new Object();
+	static Object resource2 = new Object();
+	static Object resource3 = new Object();
+	static int nRuns = 1; 
 
+	void f1() {
+		synchronized (resource1) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			synchronized (resource2) {
+				synchronized (resource3) {
+					// any code
+				}
+			}
+		}
+	}
+
+	void f2() {
+		synchronized (resource2) {
+			synchronized (resource1) {
+				// any code
+			}
+		}
+	}
+
+	void f3() {
+		synchronized (resource2) {
+			synchronized (resource3) {
+				// any code
+			}
+		}
+	}
+	@Override
+	public void run() {
+		for(int i = 0; i < nRuns; i++) {
+			f1();
+			f2();
+			f3();
+		}
+	}
 }
