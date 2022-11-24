eb7d1b65420127a4366cf124ca352c3f766a159b
Anton Balter <antbalter@gmail.com>
2022-11-24 20:53:59 +0200

    cw43

---
+++ src/telran/multithreading/MessageBox.java
@@ -0,0 +1,23 @@
+package telran.multithreading;
+
+public class MessageBox {
+	private String message;
+	public void put(String message) {
+		synchronized(this) {
+			this.message = message;
+			this.notify();
+		}
+	}
+	
+	public String get() throws InterruptedException {
+		synchronized(this) {
+			while(message == null) {
+				this.wait();
+			}
+			String res = message;
+			message = null;
+			return res;
+		}		
+	}
+
+}
