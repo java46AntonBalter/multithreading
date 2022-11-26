aa447fe220376c83659ebc09e36f2bd4912ba711
Anton Balter <antbalter@gmail.com>
2022-11-26 20:33:12 +0200

    hw43 solution

--- src/telran/multithreading/Truck.java
+++ src/telran/multithreading/Truck.java
@@ -1,37 +1,66 @@
 package telran.multithreading;
 
-public class Truck extends Thread{
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Truck extends Thread {
 	private int load;
 	private int nLoads;
 	private static long elevator1;
 	private static long elevator2;
-	private static final Object mutex = new Object();
+	public final static Lock lock = new ReentrantLock(true);
+	public final static Lock lock2 = new ReentrantLock(true);
+	private static AtomicInteger waitingCounter = new AtomicInteger(0);
+
 	public Truck(int load, int nLoads) {
 		this.load = load;
 		this.nLoads = nLoads;
 	}
+
 	@Override
 	public void run() {
 		for (int i = 0; i < nLoads; i++) {
-			loadElevator1(load);
-			loadElevator2(load);
-		}
-	}
-	 static private void loadElevator2(int load) {
-		 synchronized (mutex) {
-			{
-				elevator2 += load;
+			while (!lock.tryLock()) {
+				waitingCounter.incrementAndGet();
+			}
+			try {
+				loadElevator1(load);
+				//loadElevator2(load);
+			} finally {
+				lock.unlock();
+			}
+
+			while (!lock2.tryLock()) {
+				waitingCounter.incrementAndGet();
+			}
+			try {
+				loadElevator2(load);
+			} finally {
+				lock2.unlock();
 			}
 		}
-		
 	}
-	synchronized static private void loadElevator1(int load) {
+
+	public static int getWaitingCounter() {
+		return waitingCounter.get();
+	}
+
+	static private void loadElevator1(int load) {
+
 		elevator1 += load;
-		
 	}
+
+	static private void loadElevator2(int load) {
+
+		elevator2 += load;
+
+	}
+
 	public static long getElevator1() {
 		return elevator1;
 	}
+
 	public static long getElevator2() {
 		return elevator2;
 	}
